b48a0de6d300629e175a40e79db2f2246ae3409b
Hugox96 <alveshugo2906@gmail.com>
2022-06-02 07:51:49 -0300

    Fazendo exercicios de dicionario

--- Dicionários/exercicios Python/Situacao.py
+++ Dicionários/exercicios Python/Situacao.py
@@ -2,9 +2,9 @@ import media
 import cadastroAlunos
 def situacao(media, nome):
     if media >= 6:
-           print(nome, "aprovado")
+           print("o aluno ", nome, " foi aprovado com média: ", media)
     else:
-        print(nome, 'reprovado')
+            print("o aluno ", nome, " foi aprovado com média: ", media)
 
 
 
